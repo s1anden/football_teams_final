c6c7169a90b758841c867d6968c71b7d96ac3efa
Samantha Landen <slanden@andrew.cmu.edu>
2013-10-31 11:43:33 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+	teams = require('./routes/teams'),
+	players = require('./routes/players');
+
+var app = express();
+
+app.configure(function(){
+  app.use(express.favicon());	// Return a favicon if requested
+  app.use(express.logger('tiny'));	// Log requests to the console.log
+  app.use(express.bodyParser());	// Parse the request body into req.body object
+  app.use(express.methodOverride()); // Allows you to override HTTP methods on old browsers
+  app.use(app.router); // Do the routes defined below
+  app.use(express.static(path.join(__dirname, 'public')));	// Process static files
+});
+
