6c260755c01ecbdcabda22b7db7639fc998cb11e
Anthony Corletti <anthcor@gmail.com>
2013-11-03 18:38:23 -0500

    saving

--- models/database.js
+++ models/database.js
@@ -1,1 +1,3 @@
-[{"name":"49ers","players":["Colin Kaepernick","Vernon Davis","Justin Smith"],"coach":"Jim Harbaugh","city":"San Francisco"},{"name":"Steelers","players":["Ben Rothelisberger","Troy Polamalu"],"coach":"Mike Tomlin","city":"Pittsburgh"},{"name":"d","players":[],"coach":"d","city":"d"}]
+var data = [{"name":"49ers","players":["Colin Kaepernick","Vernon Davis","Justin Smith"],"coach":"Jim Harbaugh","city":"San Francisco"},{"name":"Steelers","players":["Ben Rothelisberger","Troy Polamalu"],"coach":"Mike Tomlin","city":"Pittsburgh"},{"name":"d","players":[],"coach":"d","city":"d"}]
+
+exports.database = data;
